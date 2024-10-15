f75cea24e521439c190a13458eaf26e9a4e06976
Diego <34121369+Diego-dot-web@users.noreply.github.com>
2024-10-14 21:44:48 -0500

    clean trash

--- src/pages/api/sendMessage.ts
+++ src/pages/api/sendMessage.ts
@@ -24,25 +24,20 @@ export async function POST(context: APIContext) {
     if (!formSchema.success) {
       throw new Error("No se pueden dejar campos en blanco");
     }
+    //send message to the email
+    try {
+      const mailData = {
+        from: import.meta.env.EMAIL,
+        to: "sugo4354@gmail.com",
+        subject: "Contacto Next",
+        text: `Hola ${name} ${email} ${tel} ${message} `,
+      };
 
-    const hasErrors = Object.values(errors).some((msg) => msg);
-    if (!hasErrors) {
-      //send message to the email
-      try {
-        const mailData = {
-          from: import.meta.env.EMAIL,
-          to: "sugo4354@gmail.com",
-          subject: "Contacto Next",
-          text: `Hola ${name} ${email} ${tel} ${message} `,
-        };
-
-        await transporter.sendMail(mailData);
-        context.redirect("/contact");
-      } catch (error: any) {
-        throw new Error(error);
-      }
+      await transporter.sendMail(mailData);
+      context.redirect("/contact");
+    } catch (error: any) {
+      throw new Error(error);
     }
-    return context.redirect("/contact");
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.message);
