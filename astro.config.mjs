dc9cdf59e13646797ed13a8494d4dea0b822e8f3
Diego <34121369+Diego-dot-web@users.noreply.github.com>
2024-10-16 11:12:19 -0500

    Deploy temprano por si acaso, foto

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,14 @@ import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
 
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 
   security: {
     checkOrigin: true,
