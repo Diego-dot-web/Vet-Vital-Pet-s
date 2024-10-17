488829d16b45e3da958edc0101db72042cf242fa
Liceth <dianapzm24@outlook.com>
2024-10-16 21:21:50 -0500

    Si no se suben los cambios no hay foto xD

--- actions/products.ts
+++ actions/products.ts
@@ -2,63 +2,61 @@ export const productos = [
   {
     imagen: "nextgard.png",
     nombre: "NexGard Antipulgas para perros de 2 a 4 Kg",
-    precio: "40.698",
+    precio: "$ 40.698",
   },
   {
     imagen: "spectre.png",
-    nombre:
-      "Nexgard Spectra Antiparasitario interno y externo para perros de 30 a 60 Kg",
-    precio: "79.764",
+    nombre: "Nexgard Spectra Antiparasitario interno y externo para perros de 30 a 60 Kg",
+    precio: "$ 79.764",
   },
   {
     imagen: "chunky.png",
     nombre: "Chunky Perros Adultos Pollo 25 Kg",
-    precio: "185.719",
+    precio: "$ 185.719",
   },
   {
     imagen: "bravecito.png",
     nombre: "Bravecto Antiparasitario Perros de 10 a 20 Kg Tableta 500 mg",
-    precio: "91.829",
+    precio: "$ 91.829",
   },
   {
     imagen: "atri.png",
-    nombre:
-      "Artri-Vet Suplemento Alimenticio para Perros y Gatos x 60 tabletas",
-    precio: "91.587",
+    nombre:"Artri-Vet Suplemento Alimenticio para Perros y Gatos x 60 tabletas",
+    precio: "$ 91.587",
   },
   {
     imagen: "gold.png",
     nombre: "Agility Gold Concentrado para perros Obesos 7 Kg",
-    precio: "143.775",
+    precio: "$ 143.775",
   },
   {
     imagen: "hills.png",
     nombre: "Hills Science Diet Gatos Adultos Optimal Care 16 Lb",
-    precio: "345.162",
+    precio: "$ 345.162",
   },
   {
     imagen: "felix.png",
     nombre: "Felix Comida Húmeda Gatos Paté Pescado y Atún 156 g",
-    precio: "5.232",
+    precio: "$ 5.232",
   },
   {
     imagen: "fresh.png",
     nombre: "Filtro para Bebedero Fuente Fresh Flow II 24695",
-    precio: "18.364",
+    precio: "$ 18.364",
   },
   {
     imagen: "rascadera.png",
     nombre: "Rascadera Mimi color Turquesa",
-    precio: "40.651",
+    precio: "$ 40.651",
   },
   {
     imagen: "maxcare.png",
     nombre: "Arena OdourLock MaxCare de 12 Kg",
-    precio: "159.900",
+    precio: "$ 159.900",
   },
   {
     imagen: "guacal.png",
     nombre: "Guacal para gato Top Load 49 x 32.5 x 25.4cm color Lila 41455",
-    precio: "170.500",
+    precio: "$ 170.500",
   },
 ];
